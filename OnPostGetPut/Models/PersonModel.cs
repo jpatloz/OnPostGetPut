2ad38b6f3b27c8e3c59a33bb14c76fe6eca38495
Jesus Patricio <jesuspatriciolozano@gmail.com>
2022-12-13 20:29:17 +0100

    Agregar archivos de proyecto.

---
+++ OnPostGetPut/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnPostGetPut.Models
+{
+    public class PersonModel
+    {
+        [BindProperty]
+        public string FirstName { get; set; }
+
+        [BindProperty]
+        public string LastName { get; set; }
+    }
+}
---
+++ OnPostGetPut/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnPostGetPut.Models;
+
+namespace OnPostGetPut.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostSubmit(PersonModel person)
+        {
+            this.Name = String.Format("Name: {0} {1}", person.FirstName, person.LastName);
+        }
+    }
+}
